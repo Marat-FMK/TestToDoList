599e27c4b5969887ca99a478466addbe6de8dcf9
MaratXcode <kosjachok8@yandex.ru>
2024-08-30 19:10:49 +0300

    work with storage manager

---
+++ TestToDoList/StorageManager.swift
@@ -0,0 +1,86 @@
+//
+//  StorageManager.swift
+//  TestToDoList
+//
+//  Created by Marat Fakhrizhanov on 30.08.2024.
+//
+
+import CoreData
+
+class StorageManager {
+    
+    static let shared = StorageManager()
+    
+    // MARK: - Core Data stack
+    var persistentContainer: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "ToDoListNew")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    private let viewContext: NSManagedObjectContext
+    
+    private init() {
+        viewContext = persistentContainer.viewContext
+    }
+    
+    // MARK: - Public Methods
+    
+    func fetchData(completion: (Result<[Task],Error>) -> Void) {
+        let fetchRequest = Task.fetchRequest()
+        
+        do {
+            let tasks = try viewContext.fetch(fetchRequest)
+            completion(.success(tasks))
+        } catch let error {
+            completion(.failure(error))
+        }
+    }
+    
+    func save(taskName: String, taskNote: String? = nil, completion: (Task) -> Void) {
+        let task = Task(context: viewContext)
+        task.title = taskName
+        task.date = Date()
+        task.note = taskNote
+        task.isDone = false
+        completion(task)
+        saveContext()
+    }
+    
+    func edit(_ task: Task, newName: String, newNoteValue: String?) {
+        task.title = newName
+        task.note = newNoteValue
+        saveContext()
+    }
+    
+    func delete(_ task: Task) {
+        viewContext.delete(task)
+        saveContext()
+    }
+    
+    func done(task: Task) {
+        task.isDone.toggle()
+        saveContext()
+    }
+    
+//    func undone(task: Task) {
+//        task.isDone = false
+//        saveContext()
+//    }
+    
+    func saveContext() {
+        if viewContext.hasChanges {
+            do {
+                try viewContext.save()
+            }catch  {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
