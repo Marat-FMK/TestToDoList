d7c25babcd9268399ad7fd876c1ddfccad86509e
MaratXcode <kosjachok8@yandex.ru>
2024-08-30 17:48:51 +0300

    work with main

---
+++ TestToDoList/TaskInfoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TaskInfoViewController.swift
+//  TestToDoList
+//
+//  Created by Marat Fakhrizhanov on 30.08.2024.
+//
+
+import UIKit
+
+class TaskInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TestToDoList/TaskListTableViewController.swift
+++ TestToDoList/TaskListTableViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TaskListTableViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
