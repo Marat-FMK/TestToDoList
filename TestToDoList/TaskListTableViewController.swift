989bf91bf2200fcb6d02ffe83bcb42b080a9bb91
MaratXcode <kosjachok8@yandex.ru>
2024-08-30 17:49:32 +0300

    work with view controllers

--- TestToDoList/TaskListTableViewController.swift
+++ TestToDoList/TaskListTableViewController.swift
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class TaskListTableViewController: UIViewController {
+class TaskListTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
