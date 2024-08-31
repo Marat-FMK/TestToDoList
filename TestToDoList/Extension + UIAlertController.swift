c4bb46af9304c64f64a68485a9b6e1433f4a1cf9
MaratXcode <kosjachok8@yandex.ru>
2024-08-31 13:57:25 +0300

    work with alert controller

---
+++ TestToDoList/Extension + UIAlertController.swift
@@ -0,0 +1,40 @@
+//
+//  Extension + UIAlertController.swift
+//  TestToDoList
+//
+//  Created by Marat Fakhrizhanov on 31.08.2024.
+//
+
+import UIKit
+
+extension UIAlertController {
+    
+    static func createAlertController(withTitle title: String) -> UIAlertController {
+        UIAlertController(title: title, message: " What do you want to do", preferredStyle: .alert)
+    }
+    
+    func action(task: Task?, completion: @escaping (String,String) -> Void) {
+        let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
+            
+            guard let newValue = self.textFields?.first?.text else { return }
+            guard !newValue.isEmpty else { return }
+            
+            guard let newNote = self.textFields?.last?.text else { return }
+            
+            completion(newValue, newNote)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+        
+        addAction(saveAction)
+        addAction(cancelAction)
+        addTextField { textField in
+            textField.placeholder = "Task"
+            textField.text = task?.title
+        }
+        addTextField {textField in
+            textField.placeholder = "Note"
+            textField.text = task?.note
+        }
+    }
+}
